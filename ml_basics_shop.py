0d111c8cc1cde93f4e8453970a4724c2df8f5991
Hari-42 <harisaranmohanathas@hotmail.com>
2026-04-08 15:18:55 +0200

    Features (X) und Zielvariable (Y) trennt

--- ml_basics_shop.py
+++ ml_basics_shop.py
@@ -1,6 +1,14 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+
 
 
 data = pd.read_csv("m245_lu02_a02_input.csv")
+X = data.drop("buy", axis=1)
+Y = data["buy"]
 
-print(data)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
